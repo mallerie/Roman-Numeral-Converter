cf04b79e029561bcfdc68f5a3608a07f07f501a7
Mallerie Shirley <mallerie@gmail.com>
2016-08-12 20:04:10 -0400

    added roman variables

--- main.js
+++ main.js
@@ -4,3 +4,16 @@ function numToRoman(number) {
   // body...
 }
 
+var M = 1000;
+var CM = 900;
+var D = 500;
+var CD = 400;
+var C = 100;
+var XC = 90;
+var L = 50;
+var XL = 40;
+var X = 10;
+var IX = 9;
+var V = 5;
+var IV = 4;
+var I = 1;
