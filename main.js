7c7f33970287f5dfd40c50c24b8858150e1baf2f
Mallerie Shirley <mallerie@gmail.com>
2016-08-14 23:41:12 -0400

    Deleted the comma variable and split the number on the empty string

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
-var M = 1000;
-var CM = 900;
-var D = 500;
-var CD = 400;
-var C = 100;
-var XC = 90;
-var L = 50;
-var XL = 40;
-var X = 10;
-var IX = 9;
-var V = 5;
-var IV = 4;
-var I = 1;
 
-var comma = ",";
 
 function numToRoman(number) {
+  var M = "1000";
+  var CM = "900";
+  var D = "500";
+  var CD = "400";
+  var C = "100";
+  var XC = "90";
+  var L = "50";
+  var XL = "40";
+  var X = "10";
+  var IX = "9";
+  var V = "5";
+  var IV = "4";
+  var I = "1";
   var stringNum = number.toString();
-    return stringNum;
+
+    return stringNum.split("");
   }
 
 
 
-function splitDigits(numString, comma){
-
-}
 
