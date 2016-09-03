54ac5ce32997e171f6053f092895ecf516d74246
Mallerie Shirley <mallerie@gmail.com>
2016-09-03 13:13:52 -0400

    Updated var to let

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 
-let romanNumerals = [["","I","II","III","IV","V","VI","VII","VIII","IX"],        
+let romanArray = [["","I","II","III","IV","V","VI","VII","VIII","IX"],        
                     ["","X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],   
                     ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],
                     ["", "M", "MM", "MMM", "MMMM", "MMMMM"]];  
 
 
 function numToRoman(number) {
-  var stringNum = number.toString().split("").reverse();
-
+  let romanNumeral = "",
+  let stringNum = number.toString().split("").reverse();
     return stringNum;
 }
 
 
 function showNum() {
-  var regularNum = document.getElementById('number-input').value;
-  var message = "You typed " + regularNum + ".";
+  let regularNum = document.getElementById('number-input').value;
+  let message = "You typed " + regularNum + ".";
 
   document.getElementById("show-num").innerHTML = message;
   
