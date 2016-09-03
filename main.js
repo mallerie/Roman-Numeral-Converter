3accec5a34a347c33ee2403fb67c787c43f61696
Mallerie Shirley <mallerie@gmail.com>
2016-09-03 12:58:23 -0400

    Added roman numeral array

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
 
+let romanNumerals = [["","I","II","III","IV","V","VI","VII","VIII","IX"],        
+                    ["","X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],   
+                    ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],
+                    ["", "M", "MM", "MMM", "MMMM", "MMMMM"]];  
+
 
 function numToRoman(number) {
-  var M = "1000";
-  var CM = "900";
-  var D = "500";
-  var CD = "400";
-  var C = "100";
-  var XC = "90";
-  var L = "50";
-  var XL = "40";
-  var X = "10";
-  var IX = "9";
-  var V = "5";
-  var IV = "4";
-  var I = "1";
   var stringNum = number.toString().split("").reverse();
 
     return stringNum;
-  }
+}
 
-  
+
 function showNum() {
   var regularNum = document.getElementById('number-input').value;
   var message = "You typed " + regularNum + ".";
