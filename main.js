12117db94f5a0cb20a78a9aa4073aab161c4fcaa
Mallerie Shirley <mallerie@gmail.com>
2016-09-03 20:10:11 -0400

    Converter functionality complete

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 
-let romanArray = [["","I","II","III","IV","V","VI","VII","VIII","IX"],        
+var romanArray = [["","I","II","III","IV","V","VI","VII","VIII","IX"],        
                     ["","X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],   
                     ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],
-                    ["", "M", "MM", "MMM", "MMMM", "MMMMM"]];  
+                    ["", "M", "MM", "MMM", "MMMM", "MMMMM", "MMMMMM"]];  
 
+var romanNumeral = "";
+var romanBackwardsArray = [];
+var digits = [];
 
-function numToRoman(number) {
-  let romanNumeral = "",
-  let stringNum = number.toString().split("").reverse();
-    return stringNum;
-}
-
-
-function showNum() {
-  let regularNum = document.getElementById('number-input').value;
-  let message = "You typed " + regularNum + ".";
-
-  document.getElementById("show-num").innerHTML = message;
+function test(argument) {
+  var n = document.getElementById('number-input').value;
+  var digits = n.split("").reverse();
+  console.log("you entered ", digits)
   
-  // return message;
+  for (var i = 0; i < digits.length; i++) {
+     romanBackwardsArray.push(romanArray[i][parseInt(digits[i])]);
+  }
 
-  // document.getElementById('show-num').innerHTML = message;
+  document.getElementById('show-num').innerHTML = romanBackwardsArray.reverse().join("");
+  romanBackwardsArray = [];
+  digits = [];
 }
+
+
 
 
 
